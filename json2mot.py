607c78a7f17329a148072ad9e082fbe0901ed2c0
aghagolz <mohammad.aghagolzadeh@here.com>
2017-03-28 12:09:56 -0500

    finished json to MOT conversion

--- json2mot.py
+++ json2mot.py
@@ -2,71 +2,52 @@ import os, json
 import pandas as pd
 import numpy as np
 from PIL import Image
+from MOT_util import haversine
 
 json_toy = 'HT085_1465209329-posecentric.json'
-param = {}
-param['pose_step'] = 50
-param['image_nrows_default'] = 1000 #None for automatic
-param['image_ncols_default'] = 1000 #None for automatic
-param['object_size'] = 100 #in auto mode, this is in meters
+output_file_path = 'det_mot.txt'
 
-with open(json_toy) as data_file:    
-    data = json.load(data_file)['fromPosePointToSamplePoints']
+param = {
+  'skip_npoints':50,
+  'pixel_size':1., #in meters
+  'object_size':10., #in meters
+  }
+zoom = 1/param['pixel_size']
 
-pose_timestamps = data.keys()
+with open(json_toy) as data_file:
+  data = json.load(data_file)['fromPosePointToSamplePoints']
 
-for t in pose_timestamps:
-    intersection_points = data[t]['samplepoints']
-    for ip in intersection_points:
-        ip_lon = float(ip['longitude'])
-        ip_lat = float(ip['latitude'])
-        ip_alt = float(ip['elevation'])
-        ip_surface_id = long(ip['roadSurfaceID'])
-        ip_LB_id = ip['boundaryCurveID']
+det_lon = np.array([float(p['longitude']) for k,v in data.iteritems() for p in v['samplepoints']])
+det_lat = np.array([float(p['latitude'])  for k,v in data.iteritems() for p in v['samplepoints']])
+det_timestamps = [k for k,v in data.iteritems() for p in v['samplepoints']]
+timestamp_to_frame_idx = dict(zip(data.keys(),range(len(data))))
 
+lat_min, lat_max = (det_lat.min(), det_lat.max())
+lon_min, lon_max = (det_lon.min(), det_lon.max())
 
-# from old script...
+w = int(haversine(lon_min,lat_min,lon_max,lat_min))
+h = int(haversine(lon_min,lat_min,lon_min,lat_max))
 
-# pose_df = pd.read_csv(input_file_path, header=None, sep=' ')
-# pose_df = pose_df.ix[::parameters['pose_step']]
+print('\tWidth= %d (meters)'%(w))
+print('\tHeight= %d (meters)'%(h))
 
-# lat = np.array(pose_df[0]) #latitude
-# lon = np.array(pose_df[1]) #longitude
-n_frames = len(pose_timestamps)
+image_nrows = h *zoom
+image_ncols = w *zoom
+print('\tZoom= %f'%(zoom))
+print('\tOutput image is %d x %d (pixels)'%(image_nrows,image_ncols))
 
-if not parameters['image_nrows_default']:
-  w = int(haversine(lon.min(),lat.min(),lon.max(),lat.min()))
-  h = int(haversine(lon.min(),lat.min(),lon.min(),lat.max()))
-  print('\tAutomatic scaling: width=%d (m), height=%d (m)'%(w,h))
-  parameters['image_nrows'] = h+parameters['object_size'] # meter-wide pixels
-  parameters['image_ncols'] = w+parameters['object_size']
-  if max(w,h)>10000:
-    return 1
-else:
-  parameters['image_nrows'] = parameters['image_nrows_default']
-  parameters['image_ncols'] = parameters['image_ncols_default']
-
-# convert (longitude, latitude) to pixels
-lat = np.floor((lat-lat.min())/(lat.max()-lat.min())*parameters['image_nrows'])+1
-lon = np.floor((lon-lon.min())/(lon.max()-lon.min())*parameters['image_ncols'])+1
-
-# synthesize multiple tracks (by translating the pose track)
-
-tracks = []
-for shift in parameters['pose_shifts']:
-tracks.append((lat+shift[0],lon+shift[1]))
-n_tracks = len(tracks)
+# map (longitude, latitude) to (row, column)
+det_row = np.floor( (det_lat-lat_min) / (lat_max-lat_min) *image_nrows ) +1
+det_col = np.floor( (det_lon-lon_min) / (lon_max-lon_min) *image_ncols ) +1
 
 # write to output
-out = np.zeros((n_frames*n_tracks,10),dtype=int)
-for frame in range(n_frames):
-for ii in range(n_tracks):
-  out[frame*n_tracks + ii,0] = frame+1
-  out[frame*n_tracks + ii,2] = tracks[ii][0][frame]#-parameters['object_size']/2
-  out[frame*n_tracks + ii,3] = tracks[ii][1][frame]#-parameters['object_size']/2
+out = np.zeros((len(det_timestamps),10),dtype=int)
+out[:,0] = [timestamp_to_frame_idx[t]+1 for t in det_timestamps]
 out[:,1] = -1
-out[:,4] = parameters['object_size']
-out[:,5] = parameters['object_size']
+out[:,2] = det_row
+out[:,3] = det_col
+out[:,4] = param['object_size'] *zoom
+out[:,5] = param['object_size'] *zoom
 out[:,6] = 1
 out[:,7] = -1
 out[:,8] = -1
