f2cd038a24ba0fe951046ec8b8b24e15e2533cb2
Mohammad Aghagolzadeh <mohammad.aghagolzadeh@here.com>
2017-04-17 17:20:46 -0500

    initial commit; time for debugging now...

--- modules/preprocess/motutil/highv1_to_mot_det.py
+++ modules/preprocess/motutil/highv1_to_mot_det.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 from haversine import haversine
 
 def highv1_to_mot_det(input_file_path, pose_filename, output_file_path, parameters):
@@ -16,42 +17,54 @@ def highv1_to_mot_det(input_file_path, pose_filename, output_file_path, paramete
   """
   zoom = 1 / parameters['pixel_size']
 
+  prefix = '/FuseToTLLAIOutput/'
+  chunk_list = [i for i in os.listdir(input_file_path+prefix) if i.isdigit()]
+  for chunk in chunk_list:
+    for filename in os.listdir(input_file_path+prefix+chunk):
+      if not filename.endswith('tllai'): continue
+      try:
+        dets = np.vstack([dets,np.loadtxt(input_file_path+prefix+chunk+'/'+filename)])
+      except NameError:
+        dets = np.loadtxt(input_file_path+prefix+chunk+'/'+filename)
+  dets = dets[:,[1,2,3,0]] #re-order columns to mimic pose data
+
+  #extract vehicle pose information
   pose = np.load(input_file_path+'/'+pose_filename)
+  timestamp_to_frame_idx = dict(enumerate(pose[:,3],start=1))
 
-  timestamps = sorted([int(k) for k in data])
-  timestamps = [t for i,t in enumerate(timestamps) if not i%parameters['step_size']]
+  #round detections' timestamps to closest ones in the pose file (nearest neighbor)
+  for i in range(dets.shape[0]):
+    dets[i,3] = pose[np.argmin(abs(pose[:,3]-dets[i,3])),3]
 
-  det_lon = np.array([float(p['longitude']) for t in timestamps for p in data['%d'%t]['samplepoints']])
-  det_lat = np.array([float(p['latitude'])  for t in timestamps for p in data['%d'%t]['samplepoints']])
-  det_timestamps = [t for t in timestamps for p in data['%d'%t]['samplepoints']]
-  timestamp_to_frame_idx = dict(zip(timestamps,range(len(timestamps))))
+  dets = np.vstack([dets,pose])
+  dets_lon = dets[:,0]
+  dets_lat = dets[:,1]
+  dets_timestamps = dets[:,3]
 
-  lat_min, lat_max = (det_lat.min(), det_lat.max())
-  lon_min, lon_max = (det_lon.min(), det_lon.max())
+  lat_min, lat_max = (dets_lat.min(), dets_lat.max())
+  lon_min, lon_max = (dets_lon.min(), dets_lon.max())
 
   w = haversine(lon_min,lat_min,lon_max,lat_min)
   h = haversine(lon_min,lat_min,lon_min,lat_max)
   image_nrows = max(h*zoom,1)
   image_ncols = max(w*zoom,1)
-  parameters['image_nrows'] = max(int(image_nrows),parameters['image_nrows'])
-  parameters['image_ncols'] = max(int(image_ncols),parameters['image_ncols'])
-  
+    
   if lat_max==lat_min:
-    det_row = np.ones_like(det_lat)
+    dets_row = np.ones_like(dets_lat)
   else:
-    det_row = (det_lat-lat_min)/(lat_max-lat_min)*image_nrows
+    dets_row = (dets_lat-lat_min)/(lat_max-lat_min)*image_nrows
 
   if lon_max==lon_min:
-    det_col = np.ones_like(det_lon)
+    dets_col = np.ones_like(dets_lon)
   else:
-    det_col = (det_lon-lon_min)/(lon_max-lon_min)*image_ncols
+    dets_col = (dets_lon-lon_min)/(lon_max-lon_min)*image_ncols
 
   # write to output
-  out = np.zeros((len(det_timestamps),10),dtype=object)
-  out[:,0] = [timestamp_to_frame_idx[t]+1 for t in det_timestamps]
+  out = np.zeros((len(dets_timestamps),10),dtype=object)
+  out[:,0] = [timestamp_to_frame_idx[t]+1 for t in dets_timestamps]
   out[:,1] = -1
-  out[:,2] = det_row
-  out[:,3] = det_col
+  out[:,2] = dets_row
+  out[:,3] = dets_col
   out[:,4] = parameters['object_size'] *zoom
   out[:,5] = parameters['object_size'] *zoom
   out[:,6] = 1
@@ -59,22 +72,7 @@ def highv1_to_mot_det(input_file_path, pose_filename, output_file_path, paramete
   out[:,8] = -1
   out[:,9] = -1
 
-  #make sure there are enough detections in this sequence
-  if out.shape[0]<parameters['min_dets']:
-    return 2
-
-  #drop detections at drop_rate rate
-  if parameters['recall']<1:
-    out = out[np.logical_or(out[:,0]<=parameters['keep'],np.random.rand(out.shape[0])<parameters['recall']),:]
-
   out = pd.DataFrame(out)
   out.to_csv(output_file_path, header=None, index=False)
 
-  if max(image_nrows,image_ncols)>10000:
-    print('\tWidth= %f (meters)'%(w))
-    print('\tHeight= %f (meters)'%(h))
-    print('\tZoom= %f'%(zoom))
-    print('\tOutput image is %d x %d (pixels)'%(image_nrows,image_ncols))
-    return 1
-  else:
-    return 0
+  return 0
