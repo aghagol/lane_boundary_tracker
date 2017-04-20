b8d3d21a0d803be5f73d52574be1f7f691a924db
Mohammad Aghagolzadeh <mohammad.aghagolzadeh@here.com>
2017-04-20 12:20:25 -0500

    improved variable naming in the track fusion module

--- modules/postprocess/postprocessing_util/tracklet_fusion.py
+++ modules/postprocess/postprocessing_util/tracklet_fusion.py
@@ -5,39 +5,43 @@ import numpy as np
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 
-def fuse(trks,param):
-	trk_id_id = {trk_id:id_id for id_id,trk_id in enumerate(sorted(set(trks[:,1])))}
+def fuse(tracks,param):
+	#Each track is associated with a lane boundary (second column in tracks array)
+	#Assign each lane boundary (lb) to a node in the track affinity graph
+	lb2node = {lb:node for node,lb in enumerate(sorted(set(tracks[:,1])))}
+	node2lb = {node:lb for lb,node in lb2node.iteritems()}
 
-	#compute track-to-track affinities
-	affinity = np.zeros((len(trk_id_id),len(trk_id_id)),dtype=int)
-	for frame in range(1,int(trks[:,0].max())+1):
-		trks_active = trks[trks[:,0]==frame,:]
-		ids = [trk_id_id[trk_id] for trk_id in trks_active[:,1]]
-		trks_pdist = squareform(pdist(trks_active[:,2:4],metric='euclidean'))
-		affinity[np.ix_(ids,ids)] += (trks_pdist<param['gating_thresh'])
-	np.fill_diagonal(affinity,0)
+	#Compute track-to-track affinities
+	A = np.zeros((len(lb2node),len(lb2node)),dtype=int)
+	for frame in range(1,int(tracks[:,0].max())+1):
+		active_tracks = tracks[tracks[:,0]==frame,:]
+		active_nodes = [lb2node[lb] for lb in active_tracks[:,1]]
+		active_nodes_pdist = squareform(pdist(active_tracks[:,2:4],metric='euclidean'))
+		A[np.ix_(active_nodes,active_nodes)] += (active_nodes_pdist<param['gating_thresh'])
+	np.fill_diagonal(A,0) #no self-loops allowed
 
-	#assign a common track id to tracks that should get fused
-	#here I use a recursive algorithm to assign the smallest id to matched tracks
-	id_map = {trk_id:trk_id for trk_id in trk_id_id}
-	for trk_id_1,id_id_1 in trk_id_id.iteritems():
-		for trk_id_2,id_id_2 in trk_id_id.iteritems():
-			if affinity[id_id_1,id_id_2]>=param['affinity_thresh']:
-				# print('\tfusing track %d and %d'%(trk_id_1,trk_id_2))
-				new_id = min(id_map[trk_id_1],id_map[trk_id_2])
-				id_map[trk_id_1] = new_id
-				id_map[trk_id_2] = new_id
-	trks[:,1] = [id_map[trk_id] for trk_id in trks[:,1]] #WARNING: over-riding track id's
+	#Mark tracks for fusion (assign a common label to multiple tracks)
+	#I use a region growing approach seeding from node 1
+	#=find connected components of graph (for now)
+	node_label = {node:node for node in node2lb}
+	for node_1 in node2lb:
+		for node_2 in node2lb:
+			if A[node_1,node_2]>=param['affinity_thresh']:
+				new_label = min(node_label[node_1],node_label[node_2])
+				node_label[node_1] = new_label
+				node_label[node_2] = new_label
+	tracks[:,1] = [node_label[lb2node[lb]] for lb in tracks[:,1]] #WARNING: over-riding lb names
 
-	#use tracking confidence scores to remove weaker tracks (duplicates)
+	#Use tracking confidence scores to remove the overlap
 	out = []
-	for frame in range(1,int(trks[:,0].max())+1):
-		trks_active = trks[trks[:,0]==frame,:]
-		for trk_id in set(trks_active[:,1]):
-			tmp = trks_active[trks_active[:,1]==trk_id,:]
-			if tmp.shape[0]>1: #check for duplicates
-				out.append(tmp[np.argmax(tmp[:,6]),:])
+	for frame in range(1,int(tracks[:,0].max())+1):
+		active_tracks = tracks[tracks[:,0]==frame,:]
+		for label in set(active_tracks[:,1]):
+			current_track = active_tracks[active_tracks[:,1]==label,:]
+			if current_track.shape[0]>1: #check for overlaps
+				#keep the tracked point with highest confidence
+				out.append(current_track[np.argmax(current_track[:,6]),:])
 			else:
-				out.append(tmp)
+				out.append(current_track)
 	out = np.vstack(out)
 	return out
