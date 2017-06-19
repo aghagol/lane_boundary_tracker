0b7282520f2e0afe4a7f9e209c6b93b55c7cadee
Mohammad Aghagolzadeh <mohammad.aghagolzadeh@here.com>
2017-06-19 10:31:48 -0500

    re-organize input data directory format

--- modules/preprocess/gen_fuse.py
+++ modules/preprocess/gen_fuse.py
@@ -15,13 +15,15 @@ from jsmin import jsmin
 import peak_finding
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input",  help="path to CNN predictions")
+parser.add_argument("--input",  help="path to data root directory")
+parser.add_argument("--images", help="path to CNN predictions")
 parser.add_argument("--output", help="path to output fuse files")
 parser.add_argument("--poses",  help="path to pose CSV files")
 parser.add_argument("--config", help="path to config file")
 parser.add_argument("--drives", help="path to drives list text-file")
 args = parser.parse_args()
 input_path = args.input+'/'
+images_path = args.input+'/'
 output_path = args.output+'/'
 poses_path = args.poses+'/'
 
@@ -40,15 +42,15 @@ fuse_fmt = ['%.16f','%.16f']
 for drive in drive_list:
   print('Working on drive %s'%drive)
 
-  meta = pd.read_csv(os.path.join(input_path+drive,'meta.csv'), skipinitialspace=True)
+  meta = pd.read_csv(os.path.join(input_path,'meta.csv'), skipinitialspace=True)
 
   for n,res in meta.iterrows():
     image_tag = '%d'%((res['time_start']+res['time_end'])/2)
     if os.path.exists(output_path+drive+'_'+image_tag+'.png.fuse'): continue
-    if not os.path.exists(os.path.join(input_path+drive,'images',res['name'])): continue
+    if not os.path.exists(os.path.join(images_path,res['name'])): continue
     print('\tworking on %s'%(res['name']))
 
-    pred_im = misc.imread(os.path.join(input_path+drive,'images',res['name']))/(2.**16-1)
+    pred_im = misc.imread(os.path.join(images_path,res['name']))/(2.**16-1)
     peaks = peak_finding.peaks_clean(pred_im, 0.3, input_as_mag=True)
     pred_im = pred_im*peaks
     ok = pred_im>0.99 # Choose your own threshold -- this isn't necessarily a good one.
